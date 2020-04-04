b59d3c6e5bb1ea0551c77fff3c0b98634c686661
fawaz <ifavaz@gmail.com>
2020-04-04 22:43:51 +0500

    vaguthu adapter working ---sort of

--- app/src/main/java/com/ifavaz/raajjenews/ThirdFragment.java
+++ app/src/main/java/com/ifavaz/raajjenews/ThirdFragment.java
@@ -4,9 +4,19 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.StrictMode;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
 
 
 /**
@@ -15,6 +25,14 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ThirdFragment extends Fragment {
+
+
+    ArrayList<String> mData = new ArrayList<String>();
+    ListView mListView;
+    ArrayAdapter<String> mAdapter;
+    Thread thread;
+
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -59,6 +77,52 @@ public class ThirdFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third, container, false);
+
+
+        View v = inflater.inflate(R.layout.fragment_third, container, false);
+
+        mListView = (ListView) v.findViewById(R.id.listme);
+
+
+        StrictMode.ThreadPolicy policy = new
+                StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        processData();
+
+
+        mAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, android.R.id.text1, mData);
+
+
+        // Assign adapter to ListView
+        mListView.setAdapter(mAdapter);
+
+
+        return v;
+
     }
+
+
+    private void processData() {
+        String URL = "https://vaguthu.mv/news";
+        try {
+            Document doc = Jsoup.connect(URL).get();
+            Elements questions = doc.select("h2,h4,h3");
+            for (Element question : questions) {
+                mData.add(question.text());
+            }
+
+            if (mData.size() == 0) {
+                mData.add("Empty result");
+            }
+
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            mData.clear();
+            mData.add("Exception: " + ex.toString());
+        }
+    }
+
 }
