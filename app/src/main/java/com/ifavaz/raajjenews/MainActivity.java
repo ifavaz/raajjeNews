3c4a85b7c7b4b79a5fabfdb778cd8f6462a2c923
fawaz <ifavaz@gmail.com>
2020-04-04 12:04:43 +0500

    hamburger finally working

--- app/src/main/java/com/ifavaz/raajjenews/MainActivity.java
+++ app/src/main/java/com/ifavaz/raajjenews/MainActivity.java
@@ -1,14 +1,64 @@
 package com.ifavaz.raajjenews;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+
+        private DrawerLayout mDrawer;
+        private Toolbar toolbar;
+        private NavigationView nvDrawer;
+
+        // Make sure to be using androidx.appcompat.app.ActionBarDrawerToggle version.
+        private ActionBarDrawerToggle drawerToggle;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            // Set a Toolbar to replace the ActionBar.
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            toolbar.setTitle("Hellow");
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menume);
+
+            // This will display an Up icon (<-), we will replace it with hamburger later
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+            // Find our drawer view
+            mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // The action bar home/up action should open or close the drawer.
+            switch (item.getItemId()) {
+                case android.R.id.home:
+                    mDrawer.openDrawer(GravityCompat.START);
+                    return true;
+            }
+
+            return super.onOptionsItemSelected(item);
+        }
+
+
+
+
+
+
+
+
+
 }
